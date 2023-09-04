54cc453973ee53cd3bcbc45ecfbb1c7e410db86b
shahd-alrahbi <138566899+shahd-alrahbi@users.noreply.github.com>
2023-09-04 20:25:33 +0400

    Initial commit

---
+++ test/contains_gold_silver_coins_test.dart
@@ -0,0 +1,8 @@
+import 'package:contains_gold_silver_coins/contains_gold_silver_coins.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
